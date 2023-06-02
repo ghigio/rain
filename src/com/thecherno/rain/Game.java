d0af2ff2674e0eae5c9cf4f5d91b59fd32270ace
Federico Ghigini <federico.ghigini@gmail.com>
2023-06-02 22:18:55 +0200

    Rendering Pixels

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -1,7 +1,6 @@
 package com.thecherno.rain;
 
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
@@ -70,9 +69,14 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+
 		Graphics g = bs.getDrawGraphics();
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
